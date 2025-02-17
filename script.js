71f4f7e45bb68628dd03e5665720e68596a85e5c
Albert Khe <akhe@olfeo.com>
2025-02-17 10:53:01 +0100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+let BACKEND_URL = "https://ucmbwipumd.us-east-1.awsapprunner.com";
+
+if (BACKEND_URL[BACKEND_URL.length - 1] === "/") {
+  BACKEND_URL = BACKEND_URL.slice(0, -1);
+}
+
+setInterval(function () {
+  fetch(`${BACKEND_URL}/time`)
+    .then((response) => response.json())
+    .then((data) => {
+      const timer = document.getElementById("timer");
+      timer.innerHTML = data.time;
+    });
+}, 1000);
